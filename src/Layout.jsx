5adf4e58463fabb361f24804e18420405fd07fba
Ruyaa Capital <admin@ruyaacapital.com>
2025-11-15 11:46:07 +0000

    Manual code change

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -256,11 +256,11 @@ export default function Layout({ children, currentPageName }) {
               <ul className="space-y-3 text-sm text-gray-400">
                 <li className="flex items-center gap-2">
                   <Phone className="w-4 h-4" />
-                  <span>+90 XXX XXX XXXX</span>
+                  <span className="">+963 940 632 191</span>
                 </li>
                 <li className="flex items-center gap-2">
                   <Mail className="w-4 h-4" />
-                  <span className="">admin@ruyaacapital.com</span>
+                  <span>info@rfttrade.com</span>
                 </li>
               </ul>
             </div>
